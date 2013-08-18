8e290518fbc53d79c9f964eed705709ffa3fc938
Edwin Park <esp@mecha1.com>
2013-08-17 23:34:10 -0400

    + ClojureScript, Processing.js

---
+++ html/hello-web.pde
@@ -0,0 +1,21 @@
+void setup() {
+  size(window.innerWidth, window.innerHeight, P3D);
+  println(jasons_game.core.parse_sentence_text("I love you"));
+}
+
+void draw() {
+  background(100);
+  
+//  pushStyle();
+  fill(0);
+  rect(0, 0, width, 100);
+//  popStyle();
+  
+//  noStroke();
+////  noFill();
+//  fill(255);
+//  lights();
+//  translate(mouseX, mouseY, 0);
+//  sphere(100);
+  
+}
--- project.clj
+++ project.clj
@@ -4,4 +4,12 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]]
-  :profiles {:dev {:dependencies [[midje "1.6-beta1"]]}})
+  :profiles {:dev {:dependencies [[midje "1.6-beta1"]]}}
+  :plugins [[lein-cljsbuild "0.3.2"]]
+  :cljsbuild {
+              :crossovers [jasons_game.core]
+              :crossover-path "src-cljs"
+              :builds [{:source-paths ["src-cljs"]
+                        :compiler {:output-to "html/js/main.js"
+                                   :optimizations :whitespace
+                                   :pretty-print true}}]})
