aa3b6c7df9a9c2efc56c714b58837690944d5064
Edwin Park <esp@mecha1.com>
2013-08-20 14:52:21 -0400

    + compojure

--- project.clj
+++ project.clj
@@ -4,14 +4,19 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [prismatic/dommy "0.1.1"]]
-  :profiles {:dev {:dependencies [[midje "1.6-beta1"]]}}
-  :plugins [[lein-cljsbuild "0.3.2"]]
-  :cljsbuild {
+                 [prismatic/dommy "0.1.1"]
+                 [compojure "1.1.5"]]
+  :plugins [[lein-cljsbuild "0.3.2"]
+            [lein-ring "0.8.5"]]
+  :ring {:handler jasons_game.routes/app}
+  :profiles {:dev {:dependencies [[midje "1.6-beta1"]
+                                  [ring-mock "0.1.5"]]}}
+  :cljsbuild {:repl-listen-port 9000
+              :repl-launch-commands {"my-launch" ["firefox" "-jsconsole" "http://localhost/my-page"]}
               :crossovers [jasons_game.core
                            jasons-game.world]
               :crossover-path "src-cljs"
               :builds [{:source-paths ["src-cljs"]
-                        :compiler {:output-to "html/js/main.js"
+                        :compiler {:output-to "resources/public/js/main.js"
                                    :optimizations :whitespace
                                    :pretty-print true}}]})
---
+++ src/jasons_game/routes.clj
@@ -0,0 +1,18 @@
+(ns jasons_game.routes
+  (:use compojure.core)
+  (:require [compojure.handler :as handler]
+            [compojure.route :as route]))
+
+(defroutes app-routes
+  (GET "/" [] "This is Jason's game")
+  (route/resources "/")
+  (route/not-found "Not Found"))
+
+(defn wrap-dir-index [handler]
+  (fn [req]
+    (handler
+      (update-in req [:uri]
+                 #(if (= "/" %) "/index.html" %)))))
+
+(def app
+  (wrap-dir-index (handler/site app-routes)))
