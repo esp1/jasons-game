e118a74f474ad3b418d6e323a4b9558d7c6a28ef
Edwin Park <esp@mecha1.com>
2013-08-22 13:10:03 -0400

    take bounds from img if available

--- src-cljs/jasons-game/ui/pjs/thing/person.cljs
+++ src-cljs/jasons-game/ui/pjs/thing/person.cljs
@@ -8,18 +8,31 @@
 
 (defmethod t/draw :person
   [person]
-  (d/translate (:location person)
-               {:stroke 0, :fill [0 255 0]}
-               (fn []
-                 (s/ellipse 0 0 diameter diameter)
-                 
-                 (if-let [img (d/load-image (:name person))]
-                   (d/translate
-                     [(- (/ (.-width img) 2)) (- (/ (.-height img) 2))]
-                     (fn []
-                       (s/image img 0 0)))))))
+  (d/translate
+    (:location person)
+    (fn []
+      ; drawing
+      (d/style
+        {:stroke 0, :fill [0 255 0]}
+        (fn []
+          (s/ellipse 0 0 diameter diameter)))
+      
+      ; image
+      (if-let [img (d/load-image (:name person))]
+        (d/translate
+          [(- (/ (.-width img) 2)) (- (/ (.-height img) 2))]
+          (fn []
+            (s/image img 0 0)))))))
 
 (defmethod t/bounds-in-local :person
   [person]
-  [(- radius) (- radius)
-   diameter diameter])
+  (if-let [img (d/load-image (:name person))]
+    ; get bounds from image
+    (let [width (.-width img)
+          height (.-height img)]
+      [(- (/ width 2)) (- (/ height 2))
+       width height])
+    
+    ; get bounds from drawing
+    [(- radius) (- radius)
+     diameter diameter]))
