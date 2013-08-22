bdf1a580e3cf06094b5a665cc5ee09faa9590a5f
Edwin Park <esp@mecha1.com>
2013-08-22 17:09:00 -0400

    adding core.async!

--- project.clj
+++ project.clj
@@ -4,13 +4,16 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [prismatic/dommy "0.1.1"]
-                 [ring-middleware-format "0.3.1"]
+                 [org.clojure/core.async "0.1.0-SNAPSHOT"]
+                 [cljs-ajax "0.1.6"]
                  [compojure "1.1.5"]
-                 [cljs-ajax "0.1.6"]]
+                 [jayq "2.4.0"]
+                 [prismatic/dommy "0.1.1"]
+                 [ring-middleware-format "0.3.1"]]
+  :repositories {"sonatype-oss-public" "https://oss.sonatype.org/content/groups/public/"}
   :plugins [[lein-cljsbuild "0.3.2"]
-            [lein-ring "0.8.5"]
-            [lein-cloverage "1.0.2"]]
+            [lein-cloverage "1.0.2"]
+            [lein-ring "0.8.5"]]
   :ring {:handler jasons-game.routes/app}
   :profiles {:dev {:dependencies [[midje "1.6-beta1"]
                                   [ring-mock "0.1.5"]]}}
